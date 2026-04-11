436ed28a2fc8ff471a70305b5204387d874a4b16
Adam Burszka <adam.burszka@student.uj.edu.pl>
2026-04-11 21:13:07 +0200

    working POC

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.InputSystem;
+using Unity.Cinemachine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField]
-    private int offsetX = 0;
+    private int offsetX = 3;
     [SerializeField]
-    private int offsetY = 0;
+    private int offsetY = 3;
 
     [SerializeField]
     private int gridSizeX = 5;
@@ -16,13 +17,21 @@ public class GridManager : MonoBehaviour
     private Tilemap grid;
     [SerializeField]
     private Tile tile;
+    [SerializeField]
+    private Camera mainCamera;
+    [SerializeField]
+    private CinemachineTargetGroup targetGroup;
+    private Transform vehicleParent;
 
     private PartData[,] partDataGrid;
 
     private InputAction clickAction;
 
+
     void Start()
     {
+        GameObject vehicleGO = new GameObject("Vehicle");
+        vehicleParent = vehicleGO.transform;
         clickAction = InputSystem.actions.FindAction("Click");
         partDataGrid = new PartData[gridSizeX, gridSizeY];
         for (int x = 0; x < gridSizeX; x++)
@@ -62,5 +71,88 @@ public class GridManager : MonoBehaviour
     public void SelectPart(PartData partData)
     {
         actPartData = partData;
+    }
+
+    public void Build()
+    {
+        // 1. Tworzymy tymczasową tablicę na obiekty fizyczne
+        GameObject[,] spawnedParts = new GameObject[gridSizeX, gridSizeY];
+        var newTargets = new System.Collections.Generic.List<CinemachineTargetGroup.Target>();
+        // 2. PIERWSZA PĘTLA: Spawnowanie wszystkich klocków
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                if (partDataGrid[x, y] != null)
+                {
+                    Vector3 worldPos = grid.CellToWorld(new Vector3Int(x + offsetX, y + offsetY, 0));
+                    worldPos.y += grid.cellSize.y / 2;
+                    worldPos.x += grid.cellSize.x / 2;
+
+                    GameObject newPart = Instantiate(partDataGrid[x, y].partPrefab, worldPos, partDataGrid[x, y].partPrefab.transform.rotation);
+
+                    Rigidbody2D rb = newPart.GetComponentInChildren<Rigidbody2D>();
+                    if (rb != null)
+                    {
+                        newTargets.Add(new CinemachineTargetGroup.Target
+                        {
+                            Object = rb.transform,
+                            Weight = 1f, // Jak bardzo ten klocek przyciąga kamerę
+                            Radius = 1f  // Obszar wokół klocka, który kamera ma widzieć
+                        });
+                    }
+
+                    newPart.transform.SetParent(vehicleParent, true);
+                    spawnedParts[x, y] = newPart;
+                }
+            }
+        }
+        targetGroup.Targets = newTargets;
+        // 3. DRUGA PĘTLA: Łączenie sąsiadów
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                if (spawnedParts[x, y] == null) continue;
+
+                // Sprawdzamy sąsiada z prawej (x + 1)
+                if (x + 1 < gridSizeX && spawnedParts[x + 1, y] != null)
+                {
+                    CreateJoint(spawnedParts[x, y], spawnedParts[x + 1, y]);
+                }
+
+                // Sprawdzamy sąsiada u góry (y + 1)
+                if (y + 1 < gridSizeY && spawnedParts[x, y + 1] != null)
+                {
+                    CreateJoint(spawnedParts[x, y], spawnedParts[x, y + 1]);
+                }
+            }
+        }
+
+        grid.ClearAllTiles(); // Czyścimy siatkę podglądu
+    }
+
+    // Pomocnicza metoda do tworzenia Jointa
+    private void CreateJoint(GameObject partA, GameObject partB)
+    {
+        // 1. Szukamy Rigidbody2D gdziekolwiek w strukturze obu klocków
+        Rigidbody2D rbA = partA.GetComponentInChildren<Rigidbody2D>();
+        Rigidbody2D rbB = partB.GetComponentInChildren<Rigidbody2D>();
+
+        // 2. Jeśli któryś klockek nie ma fizyki, nie możemy ich połączyć
+        if (rbA == null || rbB == null) return;
+
+        // 3. Dodajemy Joint bezpośrednio do obiektu, który MA RigidbodyA
+        // To gwarantuje, że Joint będzie działał, nawet jeśli RB jest w dziecku
+        FixedJoint2D joint = rbA.gameObject.AddComponent<FixedJoint2D>();
+
+        // 4. Łączymy go z RigidbodyB
+        joint.connectedBody = rbB;
+
+        // 5. Opcje stabilności
+        // joint.enableCollision = false; // Klocki nie wchodzą ze sobą w kolizję (lepsza wydajność)
+
+        // 6. Opcja zniszczalności
+        // joint.breakForce = 500f; // Jeśli chcesz, żeby pojazd mógł pękać
     }
 }

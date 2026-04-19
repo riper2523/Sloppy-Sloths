345399d81c699c88df21b617f5b6a5a2ec0049a4
typsoon <p.sitek228@gmail.com>
2026-04-19 12:51:47 +0200

    Dragging, adding and deleting points works

--- Assets/Prefabs/MapBuilder/Builders/PolygonBuilder.cs
+++ Assets/Prefabs/MapBuilder/Builders/PolygonBuilder.cs
@@ -16,6 +16,8 @@ public class PolygonBuilder : MonoBehaviour
   private Camera mainCam;
   private Collider2D splineCollider;
 
+  private Collider2D dummyCollider;
+
   private readonly List<NodeController> nodes = new();
 
   [SerializeField]
@@ -33,6 +35,8 @@ public class PolygonBuilder : MonoBehaviour
     splineCollider = GetComponent<Collider2D>();
     Debug.Log("Started");
 
+    dummyCollider = createDummyCollider();
+
     for (int i = 0; i < spline.GetPointCount(); i++)
     {
       var splinePoint = spline.GetPosition(i) + shape.transform.position;
@@ -48,12 +52,24 @@ public class PolygonBuilder : MonoBehaviour
     }
   }
 
+  CircleCollider2D createDummyCollider()
+  {
+    GameObject dummyObj = new("MouseDummyCollider");
+    dummyObj.transform.SetParent(transform); // Keep the hierarchy clean
+
+    // 2. Add a tiny circle collider
+    var dummyMouseCollider = dummyObj.AddComponent<CircleCollider2D>();
+    dummyMouseCollider.radius = 0.0001f; // Effectively a point
+    dummyMouseCollider.isTrigger = true;
+    return dummyMouseCollider;
+  }
+
   // Update is called once per frame
   void Update()
   {
     Vector2 mousePos = GetMouseWorldPos();
 
-    var closestOnCollider = GetClosestPointOnCollider(mousePos);
+    var closestOnCollider = GetClosestPointOnColliderEdge(mousePos);
 
     if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
     {
@@ -82,9 +98,46 @@ public class PolygonBuilder : MonoBehaviour
     return mainCam.ScreenToWorldPoint(mousePos3D);
   }
 
-  private Vector2 GetClosestPointOnCollider(Vector2 point)
+  private Vector2 GetClosestPointOnColliderEdge(Vector2 point)
   {
-    return splineCollider.ClosestPoint(point);
+    dummyCollider.transform.position = point;
+
+    // 2. Calculate the distance between the Polygon and the Dummy
+    ColliderDistance2D dist = splineCollider.Distance(dummyCollider);
+
+    // dist.pointA is the closest point on the EDGE of the splineCollider.
+    // Even if the mouse is inside, it returns the closest perimeter point!
+    return dist.pointA;
+  }
+
+  //Doubled triangle area 
+  private float GetProximityMetric(Vector2 a, Vector2 b, Vector2 c)
+  {
+    var c3 = (Vector3)c;
+    return Vector3.Cross((Vector3)a - c3, (Vector3)b - c3).magnitude;
+  }
+
+  private int findIndex(NodeController nodeController)
+  {
+    if (nodes.Count <= 1)
+    {
+      return nodes.Count;
+    }
+
+    var answer = 1;
+    var smallestArea = GetProximityMetric(nodes[0].getCoordinates(), nodes[1].getCoordinates(), nodeController.getCoordinates());
+
+    for (int i = 2; i <= nodes.Count; i++)
+    {
+      var currArea = GetProximityMetric(nodes[i - 1].getCoordinates(), nodes[i % nodes.Count].getCoordinates(), nodeController.getCoordinates());
+      if (currArea < smallestArea)
+      {
+        smallestArea = currArea;
+        answer = i;
+      }
+    }
+
+    return answer;
   }
 
   private void addToSpline(NodeController nodeController)
@@ -92,9 +145,10 @@ public class PolygonBuilder : MonoBehaviour
     //TODO: check whether the attached script objects change during the runtime
     Debug.Log($"Adding to spline: {nodeController.getCoordinates()}");
     //TODO: change this
-    nodes.Add(nodeController);
+    nodes.Insert(findIndex(nodeController), nodeController);
 
-    nodeController.NodeDeleted += (_, _) => removeFromSpline(nodeController); nodeController.NodeMoved += (_, _) => removeFromSpline(nodeController);
+    nodeController.NodeDeleted += (_, _) => removeFromSpline(nodeController);
+    nodeController.NodeMoved += (_, _) => rebuildTheSpline();
   }
 
   private void addToSplineAndRebuild(NodeController nodeController, bool withRebuild = true)
@@ -113,7 +167,7 @@ public class PolygonBuilder : MonoBehaviour
     spline.Clear();
     for (int i = 0; i < nodes.Count; i++)
     {
-      spline.InsertPointAt(i, nodes[i].getCoordinates() - shape.transform.position);
+      spline.InsertPointAt(i, nodes[i].getCoordinates() - (Vector2)shape.transform.position);
     }
 
     Debug.Log($"Rebuilt the spline, new spline is {spline.GetPointCount()}");
@@ -127,5 +181,13 @@ public class PolygonBuilder : MonoBehaviour
     nodes.Remove(nodeController);
     rebuildTheSpline();
     Debug.Log($"Removing from spline: {nodeController.getCoordinates()}");
+  }
+
+  void OnDestroy()
+  {
+    if (dummyCollider != null)
+    {
+      Destroy(dummyCollider.gameObject);
+    }
   }
 }

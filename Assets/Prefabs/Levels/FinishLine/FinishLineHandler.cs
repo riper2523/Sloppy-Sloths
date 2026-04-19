0eb4ebd42cc4959dbc3de0d8f60a89fd363a0339
Piotr Sitek <165384455+typsoon@users.noreply.github.com>
2026-04-19 14:50:40 +0200

    Add the finish line (#3)
    * Add the finish line
    
    Create the FinishLine prefab and write scripts that make it work
    
    * Small tweaks to the code
    
    Removed the dependency on deleted code
    
    Fixed the formatting in FinishLineHandler.cs

--- Assets/Prefabs/Levels/FinishLine/FinishLineHandler.cs
+++ Assets/Prefabs/Levels/FinishLine/FinishLineHandler.cs
@@ -4,13 +4,11 @@ using UnityEngine.Events;
 public class FinishLineHandler : MonoBehaviour
 {
     public UnityEvent finishLineCrossed;
-    private int crossedCount = 0;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Sloth") && crossedCount == 0)
+        if (other.CompareTag("Sloth"))
         {
-            crossedCount++;
             finishLineCrossed.Invoke();
         }
     }

4aba3257e536c567610b6a09b00732b7e69e08d8
B8saur <bbromblik@gmail.com>
2026-05-24 16:30:48 +0200

    Adjusted to match PR #33 requirements

--- Assets/Prefabs/MapParts/DeathZone/DeathZoneHandler.cs
+++ Assets/Prefabs/MapParts/DeathZone/DeathZoneHandler.cs
@@ -5,9 +5,6 @@ public class DeathZoneHandler : MonoBehaviour
     [SerializeField] private VoidEventChannelSO restartLevelEvent;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Sloth"))
-        {
-            restartLevelEvent.RaiseEvent();
-        }
+        restartLevelEvent.RaiseEvent();
     }
 }

069effcec6ee2cad7902fa13de9f923a97298abc
Adam Burszka <161977319+riper2523@users.noreply.github.com>
2026-06-01 13:45:35 +0200

    Fix issues (#45)
    Fix issues
    Add drag and drop
    Translation buttons in build panel 
    Add joints between layers

--- Assets/Prefabs/MainMenu/CurrentSessionSO.cs
+++ Assets/Prefabs/MainMenu/CurrentSessionSO.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CurrentSessionSO", menuName = "Scriptable Objects/Current Session")]
-public class CurrentSessionSO : ScriptableObject
+public class CurrentSessionSO : ScriptableObject, ISerializationCallbackReceiver
 {
     [Header("Session State")]
     public LevelData activeLevel;
     public ChapterData activeChapter;
     public bool returnToLevelSelection;
+    public void OnAfterDeserialize()
+    {
+        returnToLevelSelection = false;
+    }
+    public void OnBeforeSerialize()
+    {
+        if (!Application.isPlaying)
+        {
+            returnToLevelSelection = false;
+        }
+    }
 }
--- Assets/Prefabs/VehicleBuilder/Grid/TileMapClicker.cs
+++ Assets/Prefabs/VehicleBuilder/Grid/TileMapClicker.cs
@@ -4,9 +4,8 @@ using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
 using System.Threading;
 using System.Threading.Tasks;
-using Unity.VisualScripting;
 
-public class TileMapClicker : MonoBehaviour, IPointerClickHandler
+public class TileMapClicker : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public UnityEvent<Vector3Int> onTileClicked;
     public UnityEvent<Vector3Int> onTileDoubleClicked;
@@ -15,15 +14,23 @@ public class TileMapClicker : MonoBehaviour, IPointerClickHandler
     [SerializeField] private int doubleClickThreshold = 250;
     private int clickCounter = 0;
     private float lastClickTime = 0f;
+    [SerializeField] private GridManager gridManager;
+    private Camera mainCam;
+    private GameObject ghostIcon;
+    private PartData draggedPart;
+    private float lastDragEndTime = 0f;
+    private bool isDragging = false;
     void Awake()
     {
         if (tilemap == null)
         {
             tilemap = GetComponent<Tilemap>();
         }
+        mainCam = Camera.main;
     }
     public async void OnPointerClick(PointerEventData eventData)
     {
+        if (isDragging || Time.time - lastDragEndTime < 0.1f) return;
         Vector3 worldPosition = eventData.pointerCurrentRaycast.worldPosition;
         Vector3Int tilePosition = tilemap.WorldToCell(worldPosition);
         float timeSinceLastClick = Time.time - lastClickTime;
@@ -42,19 +49,66 @@ public class TileMapClicker : MonoBehaviour, IPointerClickHandler
             try
             {
                 await Task.Delay(doubleClickThreshold, cancelToken.Token);
-
                 onTileClicked.Invoke(tilePosition);
             }
             catch (TaskCanceledException)
             {
             }
-
         }
         else if (clickCounter >= 2)
         {
             cancelToken?.Cancel();
             onTileDoubleClicked.Invoke(tilePosition);
         }
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        isDragging = true;
+        cancelToken?.Cancel();
+        clickCounter = 0;
+
+        Vector3 worldPosition = eventData.pointerCurrentRaycast.worldPosition;
+        Vector3Int tilePosition = tilemap.WorldToCell(worldPosition);
+
+        draggedPart = gridManager.GrabPartFromGrid(tilePosition);
+
+        if (draggedPart != null)
+        {
+            ghostIcon = new GameObject("GridDragGhost");
+            SpriteRenderer sr = ghostIcon.AddComponent<SpriteRenderer>();
+            sr.sprite = draggedPart.partSpriteUI;
+            sr.sortingOrder = 999;
+
+            Vector3 pos = mainCam.ScreenToWorldPoint(eventData.position);
+            pos.z = 0;
+            ghostIcon.transform.position = pos;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (ghostIcon != null)
+        {
+            Vector3 pos = mainCam.ScreenToWorldPoint(eventData.position);
+            pos.z = 0;
+            ghostIcon.transform.position = pos;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isDragging = false;
+        if (ghostIcon != null) Destroy(ghostIcon);
+
+        if (draggedPart != null)
+        {
+            Vector3 worldPosition = mainCam.ScreenToWorldPoint(eventData.position);
+            worldPosition.z = 0;
+            gridManager.SelectPart(draggedPart);
+            gridManager.DropPartAtWorldPosition(worldPosition);
+            draggedPart = null;
+        }
+        lastDragEndTime = Time.time;
 
     }
 }

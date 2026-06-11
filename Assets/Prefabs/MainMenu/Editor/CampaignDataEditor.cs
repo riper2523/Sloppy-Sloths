1c3ec72ca1bb0944b3b22c7c36ff200bd7a5d626
typsoon <p.sitek228@gmail.com>
2026-06-11 13:39:57 +0200

    Fixed a small bug which caused the github actions to crash

--- Assets/Prefabs/MainMenu/Editor/CampaignDataEditor.cs
+++ Assets/Prefabs/MainMenu/Editor/CampaignDataEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
@@ -99,3 +100,4 @@ public class CampaignDataEditor : Editor
         return newData;
     }
 }
+#endif
